37139007ed676af21463a178291406d42edce382
kangeles47 <kangeles47@gmail.com>
2020-04-01 12:08:35 -0400

    Randomly assigning building attributes using CBECS 2003

--- NatlSurveyData.py
+++ NatlSurveyData.py
@@ -1,4 +1,5 @@
 from parcel import Parcel
+import random
 
 class NatlSurveyData:
 
@@ -13,14 +14,14 @@ class NatlSurveyData:
 
         # Determine the census division for the CBECS and RECS surveys:
         if self.survey == 'CBECS' or 'RECS':
-            self.census_division(parcel)
+            self.census_div(parcel)
 
         #Now call the function that populates building attributes using the CBECS:
         if self.survey == 'CBECS':
-            self.CBECS(parcel)
+            self.CBECS(self.census_div, parcel)
 
 
-    def census_division(self, parcel):
+    def census_div(self, parcel):
         # Census division for CBECS/RECS:
         if parcel.state == 'FL' or 'DE' or 'DC' or 'GA' or 'MD' or 'NC' or 'SC' or 'VA' or 'WV':
             census_div = 'South Atlantic'
@@ -35,13 +36,26 @@ class NatlSurveyData:
         return census_div
 
 
-    def CBECS(self, parcel):
+    def CBECS(self, census_div, parcel):
         if parcel.yr_built > 2012 and parcel.yr_built <= 2018:
             self.data_yr = 2018
         elif parcel.yr_built <= 2012 and parcel.yr_built > 2003:
             self.data_yr = 2012
         elif parcel.yr_built <= 2003 and parcel.yr_built > 1999:
             self.data_yr = 2003
+            # Pull the building data relevant to this survey year:
+            if census_div == 'South Atlantic':
+                wall_type = ['Brick, Stone, or Stucco', 'Concrete (Block or Poured)', 'Concrete Panels',
+                             'Siding or shingles', 'Metal Panels']
+                wall_weights = [418, 175, 16, 117, 136]
+                parcel.walls['type'] = random.choices(wall_type, wall_weights)
+                window_type = ['Multipaned windows', 'Tinted Window Glass', 'Reflective Window Glass']
+                window_weights = [379, 294, 53]
+                parcel.windows['type'] = random.choices(window_type, window_weights)
+                roof_type = ['Builtup', 'Shingles (Not Wood)', 'Metal Surfacing', 'Synthetic or Rubber',
+                             'Slate or Tile']
+                roof_weights = [211, 234, 244, 78, 66]
+                parcel.roof['type'] = random.choices(roof_type, roof_weights)
         elif parcel.yr_built <= 1999 and parcel.yr_built > 1995:
             self.data_yr = 1999
         elif parcel.yr_built <= 1995 & parcel.yr_built > 1992:
