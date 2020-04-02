842616da999e91dd4a400bff2731b7da89a56aec
kangeles47 <kangeles47@gmail.com>
2020-04-02 14:50:31 -0400

    Setting up assembly classes

--- BIM.py
+++ BIM.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class BIM:
 
     # Here we might have to write some sort of function that parses the .JSON file from the SimCenter BIM Model
@@ -9,15 +11,12 @@ class BIM:
         self.yr_built = yr_built
         self.address = address
         self.sq_ft = sq_ft
-        self.walls = {}
-        self.roof = {}
-        self.floors = {}
-        self.struct_sys = {}
-        self.ceilings = {}
-        self.footprint = {}  # in the case of parcel models, the footprint is going to be assumed as regular: Let's see if we can find data on this
-
-        # Let's say I wanted to add something to my empty dictionary for the walls:
-        self.walls['matls'] = 'concrete'
+        self.walls = []
+        self.roof = []
+        self.floors = []
+        self.struct_sys = []
+        self.ceilings = []
+        self.footprint = []  # in the case of parcel models, the footprint is going to be assumed as regular: Let's see if we can find data on this
 
         # Using basic building attributes, set up building metavariables:
         # 1) Tag the building as "commercial" or "not commercial"
@@ -35,7 +34,6 @@ class BIM:
         zipcode = int(parcel.address.split()[-1])
         BayCountyZipCodes = np.arange(32401, 32418)
         BayCountyZipCodes = np.append(BayCountyZipCodes, [32438, 32444, 32466])
-        print(BayCountyZipCodes)
 
         if zipcode in BayCountyZipCodes:
             parcel.state = 'FL'
@@ -43,9 +41,6 @@ class BIM:
         else:
             print('County and State Information not currently supported')
 
-class Walls:
 
-    def create_wall(self, bldg_model):
-        # List out the attributes we would like to capture for each wall:
-        #Data structure: I would like to go into
-        wall_dict = dict.fromkeys(['type', 'is_loadbearing', 'is_impact_resistant', 'height', 'length', 'materials','thicknesses', 'is_exterior','base_floor'])
+
+test = BIM('12345', 5, 'Hotel', 2002, "801 10th CT E Panama City 32401",'3200')
---
+++ assembly.py
@@ -0,0 +1,52 @@
+class Assembly:
+    def __init__(self):
+        self.type = None
+        self.is_loadbearing = None
+        self.length = None
+        self.matls = None
+        self.thcks = None
+        self.location = None
+
+        # List out the atrributes that every building assembly shares:
+        # data = dict.fromkeys(['type', 'is_loadbearing', 'length', 'materials','thicknesses', 'location'])
+
+
+class Wall(Assembly):
+    def __init__(self, bldg_model):
+        Assembly.__init__(self)
+        # Add in wall instance attributes:
+        self.is_impact_resistant = None
+        self.height = None
+        self.is_exterior = None
+        self.base_floor = None
+        self.top_floor = None
+
+
+class Roof(Assembly):
+    def __init__(self, bldg_model):
+        Assembly.__init__(self)
+        # Add in roof instance attributes:
+        self.pitch = None
+        self.cover = None
+        self.height = None
+
+
+class Floor(Assembly):
+    def __init__(self, bldg_model):
+        Assembly.__init__(self)
+        # Add in roof instance attributes:
+        self.is_loadbearing = True
+
+
+class Ceiling(Assembly):
+    def __init__(self, bldg_model):
+        Assembly.__init__(self)
+        # Add in roof instance attributes:
+        self.is_loadbearing = False
+
+
+class StructSys(Assembly):
+    def __init__(self, bldg_model):
+        Assembly.__init__(self)
+        # Add in roof instance attributes:
+        self.is_loadbearing = True
