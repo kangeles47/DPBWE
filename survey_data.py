644b41d48961857164d04be9eb78c8ddac12032b
kangeles47 <kangeles47@gmail.com>
2020-04-30 10:29:21 -0400

    Cleaning up CBECS rulesets and adding semantic translation for 2003

--- survey_data.py
+++ survey_data.py
@@ -1,4 +1,4 @@
-#from BIM import Parcel
+import os
 import numpy as np
 import random #switch later
 import pandas as pd
@@ -13,16 +13,13 @@ class SurveyData:
         else:
             self.survey = 'RECS - currently not supported'
 
-        print(self.survey)
-
         # Determine the census division for the CBECS and RECS surveys:
-        #if self.survey == 'CBECS' or 'RECS':
-            #census_div = self.census_division(parcel)
+        if self.survey == 'CBECS' or 'RECS':
+            census_div = self.census_division(parcel)
 
-        #Now call the function that populates building attributes using the CBECS:
+        # Call function that populates building attributes using the CBECS:
         if self.survey == 'CBECS':
-            #self.CBECS(census_div, parcel)
-            self.cbecs_attrib(parcel)
+            self.cbecs_attrib(census_div, parcel)
 
     def census_division(self, parcel):
         # Census division for CBECS/RECS:
@@ -89,11 +86,20 @@ class SurveyData:
         print(data_yr)
 
 
-    def cbecs_attrib(self, parcel):
-        # First I need to know what survey I need to access and the year constructed tag (for filtering later):
+    def cbecs_attrib(self, census_div, parcel):
+        # INFO: CBECS datasets are available for years: 2018, 2012, 2003, 1999, 1995, 1992, 1989, 1986, 1983, and 1979
+        # For in this code, we are interested in accessing information for the following building attributes:
+        # (1) Primary exterior wall material, (2) Primary roofing material, and (3) Window type
+        # To access attributes (1)-(3) code does the following:
+        # (1) Identify survey year and corresponding year identifier (this is for semantic translation purposes)
+        # (2) Populate values for Year Constructed and Square Footage to filter microdata
+        # (3) Conduct a random choice to assign attributes to the parcel
+
+        # Find the dataset year, corresponding year identifier, and value for "Year Constructed" tag:
         if parcel.yr_built <= 1989 and parcel.yr_built > 1986:
-            data_yr = '1989'
-            # Year constructed tag:
+            data_yr = 1989
+            yr_id = '4'
+            # Value for Year Constructed tag:
             if parcel.yr_built >= 1987 and parcel.yr_built <= 1989:
                 value_yrconc = 9
             elif parcel.yr_built >= 1984 and parcel.yr_built <= 1986:
@@ -116,9 +122,9 @@ class SurveyData:
                 print('CBECS year constructed code not supported')
 
         elif parcel.yr_built <= 2003 and parcel.yr_built > 1999:
-            data_yr = '2003'
-            # Populate tags for year constructed and square footage:
-            # Year constructed tag:
+            data_yr = 2003
+            yr_id = '8'
+            # Value for Year Constructed Tag
             if parcel.yr_built == 2004:
                 value_yrconc = 9
             elif parcel.yr_built >= 2000 and parcel.yr_built <= 2003:
@@ -140,9 +146,7 @@ class SurveyData:
             else:
                 print('Year Built not supported')
 
-        print(value_yrconc)
-
-        # Square footage tag:
+        # Value for square footage tag (consistent across datasets):
         if parcel.sq_ft <= 1000:
             value_sq_ft = 1
         elif parcel.sq_ft > 1000 and parcel.sq_ft <= 5000:
@@ -166,116 +170,163 @@ class SurveyData:
         else:
             print('CBECS square footage code not determined')
 
-        # Census division tag:
-        if parcel.state == 'FL' or 'DE' or 'DC' or 'GA' or 'MD' or 'NC' or 'SC' or 'VA' or 'WV':
-            census_div = 5  # South Atlantic
-            print(census_div)
-        elif parcel.state == 'AL' or 'KY' or 'MS' or 'TN':
-            census_div = 6  # East South Central
-        elif parcel.state == 'AR' or 'LA' or 'OK' or 'TX':
-            census_div = 7  # West South Central
-
         # Read in the .csv file:
-        CBECS_data = pd.read_csv('C:/Users/Karen/Desktop/1989_CBECS.csv')
-        #CBECS_data = pd.read_csv(data_yr + 'CBECS.csv')
+        path = 'C:/Users/Karen/Desktop/'
+        CBECS_data = pd.read_csv(path + str(data_yr) + '_CBECS.csv')
 
 
-        #Filter the dataset according to census division, year constructed, and square footage:
-        CBECS_data = CBECS_data.loc[(CBECS_data['CENDIV4'] == census_div) & (CBECS_data['YRCONC4'] == value_yrconc) & (CBECS_data['SQFTC4'] == value_sq_ft)]
+        # Filter the dataset according to census division, year constructed, and square footage:
+        cendiv_tag = 'CENDIV' + yr_id
+        yrc_tag = 'YRCONC' + yr_id
+        sqft_tag = 'SQFTC' + yr_id
+        CBECS_data = CBECS_data.loc[(CBECS_data[cendiv_tag] == census_div) & (CBECS_data[yrc_tag] == value_yrconc) & (CBECS_data[sqft_tag] == value_sq_ft)]
 
+        # Find the unique set of types for each attribute and define tag for associated weights:
+        wtype_tag = 'WLCNS' + yr_id
+        rtype_tag = 'RFCNS' + yr_id
+        wall_options = CBECS_data[wtype_tag].unique()
+        roof_options = CBECS_data[rtype_tag].unique()
+        wght_tag = 'ADJWT' + yr_id
 
-        wall_options = CBECS_data['WLCNS4'].unique()
-        roof_options = CBECS_data['RFCNS4'].unique()
-
+        # Find the weights for each of the attributes:
         wall_weights = []
         roof_weights = []
 
         for option in wall_options:
-            wall_weights.append(CBECS_data.loc[CBECS_data['WLCNS4'] == option, 'ADJWT4'].sum())
+            wall_weights.append(CBECS_data.loc[CBECS_data[wtype_tag] == option, wght_tag].sum())
 
         for option in roof_options:
-            roof_weights.append(CBECS_data.loc[CBECS_data['RFCNS4'] == option, 'ADJWT4'].sum())
+            roof_weights.append(CBECS_data.loc[CBECS_data[rtype_tag] == option, wght_tag].sum())
 
-        # Choose wall and roof descriptions:
+        # Choose wall and roof types:
         wall_choice = random.choices(wall_options,wall_weights)[0]
         roof_choice = random.choices(roof_options,roof_weights)[0]
 
-        if wall_choice == 1:
-            choice = 'Window/vision glass'
-        elif wall_choice == 2:
-            choice = 'Decor./construction glass'
-        elif wall_choice == 3:
-            choice = 'Concrete panels'
-        elif wall_choice == 4:
-            choice = 'Masonry'
-        elif wall_choice == 5:
-            choice = 'Siding/shingles/shakes'
-        elif wall_choice == 6:
-            choice = 'Metal panels'
-        elif wall_choice == 7:
-            choice = 'Other'
-        elif wall_choice == 8:
-            choice = 'Masonry & metal'
-        elif wall_choice == 9:
-            choice = 'Masonry & siding'
-        elif wall_choice == 10:
-            choice = 'Window glass & masonry'
-        elif wall_choice == 11:
-            choice = 'Window glass & concrete'
-        elif wall_choice == 12:
-            choice = 'Window glass & concrete'
-        elif wall_choice == 13:
-            choice = 'Window & construction glass'
-        elif wall_choice == 14:
-            choice = 'Steel frame & masonry'
-        elif wall_choice == 15:
-            choice = 'Window glass & metal'
-        elif wall_choice == 16:
-            choice = 'Concrete & siding'
+        # Conduct the semantic translations from the respective CBECS dataset
+        # Wall type descriptions:
+        if data_yr == 1989:
+            if wall_choice == 1:
+                choice = 'Window/vision glass'
+            elif wall_choice == 2:
+                choice = 'Decor./construction glass'
+            elif wall_choice == 3:
+                choice = 'Concrete panels'
+            elif wall_choice == 4:
+                choice = 'Masonry'
+            elif wall_choice == 5:
+                choice = 'Siding/shingles/shakes'
+            elif wall_choice == 6:
+                choice = 'Metal panels'
+            elif wall_choice == 7:
+                choice = 'Other'
+            elif wall_choice == 8:
+                choice = 'Masonry & metal'
+            elif wall_choice == 9:
+                choice = 'Masonry & siding'
+            elif wall_choice == 10:
+                choice = 'Window glass & masonry'
+            elif wall_choice == 11:
+                choice = 'Window glass & concrete'
+            elif wall_choice == 12:
+                choice = 'Window glass & concrete'
+            elif wall_choice == 13:
+                choice = 'Window & construction glass'
+            elif wall_choice == 14:
+                choice = 'Steel frame & masonry'
+            elif wall_choice == 15:
+                choice = 'Window glass & metal'
+            elif wall_choice == 16:
+                choice = 'Concrete & siding'
+            else:
+                print('Wall construction not supported')
+        elif data_yr == 2003:
+            if wall_choice == 1:
+                choice = 'Brick, stone, or stucco'
+            elif wall_choice == 2:
+                choice = 'Pre-cast concrete panels'
+            elif wall_choice == 3:
+                choice = 'Concrete block or poured concrete'
+            elif wall_choice == 4:
+                choice = 'Siding, shingles, tiles, or shakes'
+            elif wall_choice == 5:
+                choice = 'Sheet metal panels'
+            elif wall_choice == 6:
+                choice = 'Window or vision glass'
+            elif wall_choice == 7:
+                choice = 'Decorative or construction glass'
+            elif wall_choice == 8:
+                choice = 'No one major type'
+            elif wall_choice == 9:
+                choice = 'Other'
         else:
-            print('Wall construction not supported')
+            print('Survey year currently not supported')
 
         print(choice)
 
+        # Assign wall type description to every exterior wall for the parcel:
         for lst in range(0, 4):  # starting with four walls per floor
             for index in range(0, len(parcel.walls)):  # for every list (story)
                 parcel.walls[index][lst].type = choice
 
-        if roof_choice == 1:
-            parcel.roof.cover = 'Wooden materials'
-        elif roof_choice == 2:
-            parcel.roof.cover = 'Slate or tile'
-        elif roof_choice == 3:
-            parcel.roof.cover = 'Shingles (not wood)'
-        elif roof_choice == 4:
-            parcel.roof.cover = 'Built-up'
-        elif roof_choice == 5:
-            parcel.roof.cover = 'Metal surfacing'
-        elif roof_choice == 6:
-            parcel.roof.cover = 'Single/multiple ply'
-        elif roof_choice == 7:
-            parcel.roof.cover = 'Concrete roof'
-        elif roof_choice == 8:
-            parcel.roof.cover = 'Other'
-        elif roof_choice == 9:
-            parcel.roof.cover = 'Metal & rubber'
-        elif roof_choice == 10:
-            parcel.roof.cover = 'Cement & asphalt'
-        elif roof_choice == 11:
-            parcel.roof.cover = 'Composite'
-        elif roof_choice == 12:
-            parcel.roof.cover = 'Glass'
-        elif roof_choice == 13:
-            parcel.roof.cover = 'Shingles & metal'
-        elif roof_choice == 14:
-            parcel.roof.cover = 'Slate & built-up'
-        elif roof_choice == 15:
-            parcel.roof.cover = 'Built-up & metal'
-        elif roof_choice == 16:
-            parcel.roof.cover = 'Built-up & s/m ply'
+        # Roof type descriptions:
+        if data_yr == 1989:
+            if roof_choice == 1:
+                parcel.roof.cover = 'Wooden materials'
+            elif roof_choice == 2:
+                parcel.roof.cover = 'Slate or tile'
+            elif roof_choice == 3:
+                parcel.roof.cover = 'Shingles (not wood)'
+            elif roof_choice == 4:
+                parcel.roof.cover = 'Built-up'
+            elif roof_choice == 5:
+                parcel.roof.cover = 'Metal surfacing'
+            elif roof_choice == 6:
+                parcel.roof.cover = 'Single/multiple ply'
+            elif roof_choice == 7:
+                parcel.roof.cover = 'Concrete roof'
+            elif roof_choice == 8:
+                parcel.roof.cover = 'Other'
+            elif roof_choice == 9:
+                parcel.roof.cover = 'Metal & rubber'
+            elif roof_choice == 10:
+                parcel.roof.cover = 'Cement & asphalt'
+            elif roof_choice == 11:
+                parcel.roof.cover = 'Composite'
+            elif roof_choice == 12:
+                parcel.roof.cover = 'Glass'
+            elif roof_choice == 13:
+                parcel.roof.cover = 'Shingles & metal'
+            elif roof_choice == 14:
+                parcel.roof.cover = 'Slate & built-up'
+            elif roof_choice == 15:
+                parcel.roof.cover = 'Built-up & metal'
+            elif roof_choice == 16:
+                parcel.roof.cover = 'Built-up & s/m ply'
+            else:
+                print('Roof construction not supported')
+        elif data_yr == 2003:
+            if roof_choice == 1:
+                parcel.roof.cover = 'Built-up'
+            elif roof_choice == 2:
+                parcel.roof.cover = 'Slate or tile shingles'
+            elif roof_choice == 3:
+                parcel.roof.cover = 'Wood shingles/shakes/other wood'
+            elif roof_choice == 4:
+                parcel.roof.cover = 'Asphalt/fiberglass/other shingles'
+            elif roof_choice == 5:
+                parcel.roof.cover = 'Metal surfacing'
+            elif roof_choice == 6:
+                parcel.roof.cover = 'Plastic/rubber/synthetic sheeting'
+            elif roof_choice == 7:
+                parcel.roof.cover = 'Concrete'
+            elif roof_choice == 8:
+                parcel.roof.cover = 'No one major type'
+            elif roof_choice == 9:
+                parcel.roof.cover = 'Other'
         else:
-            print('Roof construction not supported')
+            print('Survey year currently not supported')
 
+        print(parcel.roof.cover)
 
 
 #Let's play with the file:
