6123bdc04758826c9a39df5d43623366ab9aab77
kangeles47 <kangeles47@gmail.com>
2020-05-01 11:29:04 -0400

    Define bounding box and filter through footprint data

--- roughness_calc.py
+++ roughness_calc.py
@@ -1,4 +1,5 @@
 from shapely.geometry import Point, Polygon
+import geopandas as gpd
 import numpy as np
 import matplotlib.pyplot as plt
 from BIM import Parcel
@@ -13,25 +14,54 @@ class Wind:
 class Site:
 
     def __init__(self, parcel, footprints, wind_direction):
-        self.location = parcel.location
+        self.location = Point(parcel.lon, parcel.lat)
         self.terrain = self.roughness_calc(parcel, footprints, wind_direction)
 
 
     def roughness_calc(self, parcel, footprints, wind_direction):
         # (1) Find the parcel's centroid - this will be the origin for the z0 calculation:
         originz = parcel.footprint['geometry'].centroid # Parcel footprint is a Polygon type
-        # (2) Define a bounding box for a fetch length and wind direction:
+        x1,y1 = parcel.footprint['geometry'].exterior.xy
+        # (2) Define a bounding box for a given fetch length and wind direction:
         if wind_direction == 0:
-            # Define points for the Polygon (starting with a rectangle here):
-            p1 = Point(originz)
+            fetch = 0.001
+            # Define points for site area Polygon object (starting with a rectangle here):
+            p1 = Point(originz.x, originz.y+fetch)
+            p2 = Point(originz.x, originz.y-fetch)
+            p3 = Point(originz.x-fetch, originz.y-fetch)
+            p4 = Point(originz.x-fetch, originz.y+fetch)
+            # Create Polygon object:
+            site_poly = Polygon([p1, p2, p3, p4])
+            x,y = site_poly.exterior.xy
+            plt.plot(x, y, x1, y1)
+            plt.show()
         elif wind_direction == 90:
-            p1 = Point(originz)
+            pass
+
+        # (3) Identify all footprints within the bounding box:
+        # Create an empty list to store footprints:
+        fetch_bldg = []
+        for row in range(0, len(data["geometry"])):
+            # Use building footprint centroid for point of reference:
+            bldg_centroid = data['geometry'][row].centroid
+            # Check if building footprint is within the site area polygon:
+            if bldg_centroid.within(site_poly):
+                fetch_bldg.append(data['geometry'][row])
+            else:
+                pass
+
         return originz
 
 
 # Identify the parcel:
-test = Parcel('12345', 4, 'Financial', 1989, '1002 23RD ST W PANAMA CITY 32405', 41134)
+lon = -85.676188
+lat = 30.190142
+test = Parcel('12345', 4, 'Financial', 1989, '1002 23RD ST W PANAMA CITY 32405', 41134, lon, lat)
 # Create an instance of the site class:
 wind_direction = 0
+jFile = 'C:/Users/Karen/Desktop/BayCounty.geojson'
+data = gpd.read_file(jFile)
+# data is a DataFrame object with column label = ['geometry'] and indexes = [0: end]
+# Accessing a specific Polygon object then requires: data['geometry'][index]
 
-site = Site(test, footprints, wind_direction)
+site = Site(test, data, wind_direction)
