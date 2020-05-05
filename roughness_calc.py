021b78f22443dcb217df667a9b883d7572425b64
kangeles47 <kangeles47@gmail.com>
2020-05-05 11:59:45 -0400

    Cleaning up pulling parcels within bounds

--- roughness_calc.py
+++ roughness_calc.py
@@ -30,10 +30,8 @@ class Site:
         fetch = 0.007
         # Create circle with radius = fetch length:
         circ = originz.buffer(fetch, resolution=200)
-        lon_circ, lat_circ = circ.exterior.xy
-        bounds = []
         # Create half-circle corresponding to the given wind direction:
-        if wind_direction == 0 or 180:
+        if wind_direction == 0 or wind_direction == 180:
             # Define boundary line:
             bline = LineString([(originz.x, originz.y + fetch), (originz.x, originz.y - fetch)])
             # Split the circle into two polygons
@@ -46,13 +44,14 @@ class Site:
                     bounds = circ_split[0]
                 else:
                     bounds = circ_split[1]
-            if wind_direction == 180:
+            elif wind_direction == 180:
                 # For 180 degrees, max longitude (x) in the Polygon we want should be larger than bldg centroid x
                 if max(x1) > originz.x:
                     bounds = circ_split[0]
                 else:
                     bounds = circ_split[1]
-        elif wind_direction == 90 or 270:
+        elif wind_direction == 90 or wind_direction == 270:
+            print(wind_direction)
             # Define boundary line:
             bline = LineString([(originz.x-fetch, originz.y), (originz.x+fetch, originz.y)])
             # Split the circle into two polygons
@@ -65,40 +64,39 @@ class Site:
                     bounds = circ_split[0]
                 else:
                     bounds = circ_split[1]
-            if wind_direction == 270:
+            elif wind_direction == 270:
                 # For 270 degrees, max latitude (y) in the Polygon we want should be larger than bldg centroid y
                 if max(y1) > originz.y:
                     bounds = circ_split[0]
                 else:
                     bounds = circ_split[1]
-        elif wind_direction == 45 or 225:
+        elif wind_direction == 45 or wind_direction == 225:
             # Define boundary line:
-            rad1 = math.pi*(3/4)
-            rad2 = math.pi*(7/4)
-            bline = LineString([(originz.x-fetch*math.cos(rad1), originz.y+fetch*math.sin(rad1)), (originz.x+fetch*math.cos(rad2), originz.y-fetch*math.sin(rad2))])
+            rad = math.pi*(1/4)
+            bline = LineString([(originz.x-fetch*math.cos(rad), originz.y+fetch*math.sin(rad)), (originz.x+fetch*math.cos(rad), originz.y-fetch*math.sin(rad))])
             # Split the circle into two polygons
             circ_split = split(circ, bline)
+            print(len(circ_split))
             # Choose the first polygon to do a check and assign bounds for the wind direction:
             x1, y1 = circ_split[0].exterior.xy
             if wind_direction == 45:
                 # For 45 degrees, use point in upper left-hand quadrant:
                 # Polygon we want will have a minimum longitude that is smaller than the longitude for this point
-                if min(x1) < originz.x-fetch*math.cos(rad1):
+                if min(x1) < originz.x-fetch*math.cos(rad):
                     bounds = circ_split[0]
                 else:
                     bounds = circ_split[1]
-            if wind_direction == 225:
+            elif wind_direction == 225:
                 # For 225 degrees, use point in lower right-hand quadrant:
                 # Polygon we want will have a maximum longitude that is larger than the longitude for this point
-                if max(x1) > originz.x+fetch*math.cos(rad2):
+                if max(x1) > originz.x+fetch*math.cos(rad):
                     bounds = circ_split[0]
                 else:
                     bounds = circ_split[1]
         elif wind_direction == 135 or 315:
             # Define boundary line:
-            rad1 = math.pi*(1/4)
-            rad2 = math.pi*(5/4)
-            bline = LineString([(originz.x+fetch*math.cos(rad1), originz.y+fetch*math.sin(rad1)), (originz.x-fetch*math.cos(rad2), originz.y-fetch*math.sin(rad2))])
+            rad = math.pi*(1/4)
+            bline = LineString([(originz.x+fetch*math.cos(rad), originz.y+fetch*math.sin(rad)), (originz.x-fetch*math.cos(rad), originz.y-fetch*math.sin(rad))])
             # Split the circle into two polygons
             circ_split = split(circ, bline)
             # Choose the first polygon to do a check and assign bounds for the wind direction:
@@ -106,14 +104,14 @@ class Site:
             if wind_direction == 135:
                 # For 135 degrees, use point in upper right-hand quadrant:
                 # Polygon we want will have a maximum longitude that is larger than the longitude for this point
-                if max(x1) > originz.x+fetch*math.cos(rad1):
+                if max(x1) > originz.x+fetch*math.cos(rad):
                     bounds = circ_split[0]
                 else:
                     bounds = circ_split[1]
-            if wind_direction == 315:
+            elif wind_direction == 315:
                 # For 315 degrees, use point in lower left-hand quadrant:
                 # Polygon we want will have a minimum longitude that is smaller than the longitude for this point
-                if min(x1) < originz.x-fetch*math.cos(rad2):
+                if min(x1) < originz.x-fetch*math.cos(rad):
                     bounds = circ_split[0]
                 else:
                     bounds = circ_split[1]
@@ -121,19 +119,20 @@ class Site:
         # Create polygon object with the selected points:
         #site_geom = Polygon(bounds)
         xsite, ysite = bounds.exterior.xy
-        plt.plot(x1, y1, xp, yp, lon_circ, lat_circ, xsite, ysite)
-        plt.show()
+        plt.plot(xp, yp, xsite, ysite)
+        #plt.show()
+
         # (3) Identify all buildings within the bounding box:
         # Read in parcel data:
         parcel_data = pd.read_csv('C:/Users/Karen/PycharmProjects/DPBWE/Datasets/Parcels/CedarsCrossing.csv')
-        # Create an empty list to store identified parcels:
+        # Create an empty list to store identified buildings:
         fetch_bldgs = []
-        # Create point objects for each parcel using its longitude and latitude and check if point is within bounding box:
+        # Create point objects for each building using longitude and latitude and check if point is within bounding box:
         for row in range(0, len(parcel_data)):
             bldg_point = Point(parcel_data['Longitude'][row], parcel_data['Latitude'][row])
-            if bldg_point.within(site_poly):
+            if bldg_point.within(bounds):
                 # If the building is in the bounding box, create a Parcel Instance:
-                pid = parcel_data['Parcel ID'][row] # Alternatively could use parcel_data.loc[row][1], but let's not assume data is in a specific order
+                pid = parcel_data['Parcel ID'][row]
                 num_stories = parcel_data['Stories'][row]
                 occupancy = parcel_data['Use Code'][row]
                 yr_built = parcel_data['Year Built'][row]
@@ -143,23 +142,11 @@ class Site:
                 lat = parcel_data['Latitude'][row]
                 new_parcel = Parcel(pid, num_stories, occupancy, yr_built, address, sq_ft, lon, lat)
                 fetch_bldgs.append(new_parcel)
+                xparcel,yparcel = new_parcel.footprint["geometry"].exterior.xy
+                plt.plot(xparcel, yparcel)
             else:
                 pass
-
-
-        # (3) Identify all footprints within the bounding box:
-        # Create an empty list to store footprints:
-        #fetch_bldg = []
-        #for row in range(0, len(data["geometry"])):
-            # Use building footprint centroid for point of reference:
-            #bldg_centroid = data['geometry'][row].centroid
-            # Check if building footprint is within the site area polygon:
-            #if bldg_centroid.within(site_poly):
-                #fetch_bldg.append(data['geometry'][row])
-                #xnew,ynew = data['geometry'][row].exterior.xy
-                #plt.plot(xnew, ynew)
-            #else:
-                #pass
+        # Now that we've identified all parcels, plot for confirmation:
         plt.show()
 
         # (4) Buildings within bounding box: interested in their 1) height and 2) surface area
@@ -188,7 +175,7 @@ lon = -85.676188
 lat = 30.190142
 test = Parcel('12345', 4, 'Financial', 1989, '1002 23RD ST W PANAMA CITY 32405', 41134, lon, lat)
 # Create an instance of the site class:
-wind_direction = 0
+wind_direction = 315
 jFile = 'C:/Users/Karen/Desktop/BayCounty.geojson'
 data = gpd.read_file(jFile)
 # data is a DataFrame object with column label = ['geometry'] and indexes = [0: end]
