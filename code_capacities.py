a9a403f872b3d35d63777b2e43fcbff46ad4f55c
kangeles47 <kangeles47@gmail.com>
2020-06-24 19:54:33 -0400

    Refactoring roof similitude with new file paths

--- code_capacities.py
+++ code_capacities.py
@@ -11,14 +11,19 @@ from code_pressures import PressureCalc
 #   b) Conduct the necessary similitude mappings
 #   c) Assign pressures for each zone to the appropriate locations for the building model
 def get_roof_uplift_pressure(edition, h_bldg, length, exposure, wind_speed, direction, pitch):
-    # Step 1: Determine the use case:
+    # Step 1: Determine which reference building is needed and the file path:
+    pressures = PressureCalc()  # Create an instance of PressureCalc to pull reference building parameters
+    if direction == 'parallel' or (direction == 'normal' and pitch < 10):
+        ref_exposure, ref_hstory, ref_hbldg, ref_pitch, ref_speed, ref_cat, hpr, h_ocean, encl_class = pressures.ref_bldg()
+        file_path = 'D:/Users/Karen/Documents/Github/DPBWE/Similitude Parameters/Roof_MWFRS/RBLDG1/'
+    # Step 2: Determine the use case:
     ratio = h_bldg/length
     if edition == 'ASCE 7-88' or edition == 'ASCE 7-93':
         if direction == 'parallel':
             if ratio <= 2.5:
                 use_case = 3
             elif ratio > 2.5:
-                use_case  = 4
+                use_case = 4
         elif direction == 'normal':
             pass
     else:
@@ -29,51 +34,40 @@ def get_roof_uplift_pressure(edition, h_bldg, length, exposure, wind_speed, dire
                 use_case = 2
         elif direction == 'normal' and pitch >= 10:
             pass
-    # Step 2: Determine which "family" of building codes will be needed (if necessary):
+    # Step 3: Determine which "family" of building codes will be needed (if necessary):
     if edition == 'ASCE 7-02' or edition == 'ASCE 7-05':
         edition = 'ASCE 7-98'
     elif edition == 'ASCE 7-88':
         edition = 'ASCE 7-93'
-    # Step 3: Extract the respective reference pressure for the use case:
-    file_path = 'D:/Users/Karen/Documents/Github/DPBWE/Similitude Parameters/Roof_MWFRS/Roof_MWFRS_'
-    if use_case == 1:
-        pref = pd.read_csv(file_path + 'ref1.csv', index_col='Edition').loc[[edition], :]
-    elif use_case == 2:
-        pref = pd.read_csv(file_path + 'ref2.csv', index_col='Edition').loc[[edition], :]
-    elif use_case == 3:
-        pref = pd.read_csv(file_path + 'ref3.csv', index_col='Edition').loc[[edition], :]
-    elif use_case == 4:
-        pref = pd.read_csv(file_path + 'ref4.csv', index_col='Edition').loc[[edition], :]
-    # Step 4: Filter out any zones that are not needed for use cases 1 and 2:
+    # Step 4: Extract the respective reference pressure for the use case:
+    pref = pd.read_csv(file_path + 'ref' + str(use_case) + '.csv', index_col='Edition').loc[[edition], :]
+    # Step 5: Filter out any zones that are not needed for use cases 1 and 2:
     if use_case == 1 and ratio == 0.5:
         pref = pref[pref.columns[0:3]]  # Case with only 3 zones
     elif use_case == 2 and ratio == 2.0:
         pref = pref[pref.columns[0:1]] # Case with only 1 zone
-    # Similitude parameters require comparison to a reference building: Note: not technically needed as long as we know what the correct reference building is
-    pressures = PressureCalc()
-    ref_exposure, ref_hstory, ref_hbldg, ref_pitch, ref_speed, ref_cat, hpr, h_ocean, encl_class = pressures.ref_bldg()
     # Step 5: Extract similitude parameters for wind speed, height, and exposure
     # Similitude in wind speed:
     if wind_speed == ref_speed:
         vfactor = 0.0
     else:
         if edition == 'ASCE 7-93':
-            vfactor = pd.read_csv(file_path + 'v3.csv', index_col='Edition')[str(wind_speed)][edition]
+            vfactor = pd.read_csv(file_path + 'v93.csv', index_col='Edition')[str(wind_speed)][edition]
         else:
-            vfactor = pd.read_csv(file_path + 'v1.csv', index_col='Edition')[str(wind_speed)][edition] # Leave here for now, for regional simulation will probably want to load everything somewhere outside
+            vfactor = pd.read_csv(file_path + 'v.csv', index_col='Edition')[str(wind_speed)][edition] # Leave here for now, for regional simulation will probably want to load everything somewhere outside
     # Similitude in height:
     if h_bldg == ref_hbldg:
         hfactor = 0.0
     else:
         if edition == 'ASCE 7-93':
-            hfactor = pd.read_csv(file_path + 'h_93.csv')[str(h_bldg) + ' ft'][0]
+            hfactor = pd.read_csv(file_path + 'h93.csv')[str(h_bldg) + ' ft'][0]
         else:
             hfactor = pd.read_csv(file_path + 'h.csv')[str(h_bldg) + ' ft'][0]
     # Similitude in exposure categories:
     if exposure == ref_exposure:
         efactor = 0.0
     else:
-        efactor = pd.read_csv(file_path + 'exp_' + edition[-2:] + '.csv', index_col='Height in ft')[exposure][h_bldg]
+        efactor = pd.read_csv(file_path + 'e' + edition[-2:] + '.csv', index_col='Height in ft')[exposure][h_bldg]
     # Step 6: Apply the similitude parameters to get the final pressures for each zone:
     factor_lst = [vfactor, hfactor, efactor]
     psim = pref.loc[edition]
