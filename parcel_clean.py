c09803c1930195323eb4f169c6a346b376aea837
kangeles47 <kangeles47@gmail.com>
2020-10-29 09:48:38 -0400

    Integrating disaster permit data for all parcels

--- parcel_clean.py
+++ parcel_clean.py
@@ -21,17 +21,20 @@ df = df.drop(c_indices)
 df = df.reset_index(drop=True)
 # Step 3: Find the number of occurrences for a given column tag:
 # Use Code
-use_count = df['Use Code'].value_counts()  # Stores, hotels, office bldg
+print('----------------Overview of Typologies in Panama City Beach and Mexico Beach:------------------')
+print(df['Use Code'].value_counts())  # Stores, hotels, office bldg
 # Roof cover type
-rcover_count = df['Roof Cover'].value_counts()  # Modular mt, stand seam, built-up, etc.
+print('Roof Cover type:')
+print(df['Roof Cover'].value_counts())  # Modular mt, stand seam, built-up, etc.
 # Number of stories
+print('Number of Stories:')
 story_count = df['Stories'].value_counts()  # 1, 2, 1.5, 3, 5 (12)
-# Step 4: Merge the building permit data:
+# Step 4: Merge the parcel permit information:
 col_names2 = ['Parcel Id', 'Address', 'Permit Number']
 df2 = pd.read_csv('D:/Users/Karen/Documents/GitHub/DPBWE/CommParcelsPermits.csv', header=0, names=col_names2)
 df2 = df2.drop_duplicates()
 df2['Permit Number'] = df2['Permit Number'].apply(eval)
-# Step 5: Use the parcel number to merge the permit data:
+# Step 5: Use the parcel number to merge the permit information:
 permit_data = []
 dis_permit = []
 for row in range(0, len(df['Parcel Id'])):
@@ -54,53 +57,53 @@ for row in range(0, len(df['Parcel Id'])):
 df['Permit Number'] = permit_data
 df['Disaster Permit'] = dis_permit
 print(df['Disaster Permit'].value_counts())
-# Step 6: Let's figure out the typologies of the buildings with permits:
-damage_indices = df.index[df['Disaster Permit'] == True].to_list()
-df_damage = df.loc[damage_indices]
-# Reset the index:
-df_damage = df_damage.reset_index(drop=True)
-print(df_damage['Roof Cover'].value_counts())
-print(df_damage['Stories'].value_counts())
-print(df_damage['Frame Type'].value_counts())
-print(df_damage['Exterior Walls'].value_counts())
-plt.figure()
-ax = df_damage['Roof Cover'].value_counts().plot.bar(x='Num of Stories', y='Num of Occurrences')
-plt.xlabel('Roof Cover Type')
-plt.ylabel('Number of Occurrences')
-plt.show()
-# Step 7: Bring in the Permit Descriptions:
+# Step 6: Bring in the Permit Subtypes and Permit Descriptions:
 df_18 = pd.read_excel('D:/Users/Karen/Documents/GitHub/DPBWE/BayCountyMichael_Permits.xlsx', sheet_name='Sheet1')
 df_19 = pd.read_excel('D:/Users/Karen/Documents/GitHub/DPBWE/BayCountyMichael_Permits.xlsx', sheet_name='Sheet2')
 df_20 = pd.read_excel('D:/Users/Karen/Documents/GitHub/DPBWE/BayCountyMichael_Permits.xlsx', sheet_name='Sheet3')
 # Setting up two lists: all_permit_desc and permit_desc b/c some parcels have more than one Disaster permit
-all_permit_desc = []
-all_permit_type = []
-for parcel in range(0, len(df_damage['Parcel Id'])):
-    plist = df_damage['Permit Number'][parcel]
-    permit_desc = []
-    type_desc = []
-    for p in plist:
-        if 'DIS' in p:
-            if 'DIS18' in p:
-                row = df_18.index[df_18['Permit Number'] == p].to_list()
-                desc = df_18['DESCRIPTION'][row]
-                tdesc = df_18['PERMITSUBTYPE'][row]
-            elif 'DIS19' in p:
-                row = df_19.index[df_19['Permit Number'] == p].to_list()
-                desc = df_19['DESCRIPTION'][row]
-                tdesc = df_19['PERMITSUBTYPE'][row]
-            elif 'DIS20' in p:
-                row = df_20.index[df_20['Permit Number'] == p].to_list()
-                desc = df_20['DESCRIPTION'][row]
-                tdesc = df_20['PERMITSUBTYPE'][row]
-            permit_desc.append(desc)
-            type_desc.append(tdesc)
-        else:
-            pass
-    all_permit_desc.append(permit_desc)
-    all_permit_type.append(type_desc)
+dis_permit_desc = []
+dis_permit_type = []
+for parcel in range(0, len(df['Parcel Id'])):
+    plist = df['Permit Number'][parcel]
+    if plist == 'N/A':
+        dis_permit_desc.append('N/A')
+        dis_permit_type.append('N/A')
+    else:
+        permit_desc = []
+        type_desc = []
+        for p in plist:
+            if 'DIS' in p:
+                if 'DIS18' in p:
+                    row = df_18.index[df_18['Permit Number'] == p].to_list()
+                    desc = df_18['DESCRIPTION'][row]
+                    tdesc = df_18['PERMITSUBTYPE'][row]
+                elif 'DIS19' in p:
+                    row = df_19.index[df_19['Permit Number'] == p].to_list()
+                    desc = df_19['DESCRIPTION'][row]
+                    tdesc = df_19['PERMITSUBTYPE'][row]
+                elif 'DIS20' in p:
+                    row = df_20.index[df_20['Permit Number'] == p].to_list()
+                    desc = df_20['DESCRIPTION'][row]
+                    tdesc = df_20['PERMITSUBTYPE'][row]
+                permit_desc.append(desc)
+                type_desc.append(tdesc)
+            else:
+                pass
+        dis_permit_desc.append(permit_desc)
+        dis_permit_type.append(type_desc)
 # Add permit descriptions and subtypes
-df_damage['Permit Type'] = all_permit_type
-df_damage['Permit Description'] = all_permit_desc
-# Step 8: Let's see what kind of roof cover damage we can pull for specific typologies:
-print('a')
+df['Disaster Permit Type'] = dis_permit_type
+df['Disaster Permit Description'] = dis_permit_desc
+# Step 7: Let's figure out the typologies of the buildings with permits:
+damage_indices = df.index[df['Disaster Permit'] == True].to_list()
+df_damage = df.loc[damage_indices]
+# Reset the index:
+df_damage = df_damage.reset_index(drop=True)
+print('-----------------------Overview of Damaged Building Typologies-----------------------')
+print('Roof Covers:')
+print(df_damage['Roof Cover'].value_counts())
+print('Stories:')
+print(df_damage['Stories'].value_counts())
+print('Frame Type:')
+print(df_damage['Frame Type'].value_counts())
