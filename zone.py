78291132070e2db56112558be3f88b415ab32545
kangeles47 <kangeles47@gmail.com>
2020-12-02 22:49:41 -0500

    renaming

--- zone.py
+++ zone.py
@@ -1,18 +1,16 @@
 import numpy as np
-from math import sqrt, pi, sin, atan2, degrees, cos, radians
+from math import sqrt, pi, sin, atan2, degrees
 import geopandas as gpd
 from shapely.geometry import Point, Polygon, LineString
-from shapely.ops import split
 from shapely import affinity
 from scipy import spatial
-from scipy.interpolate import griddata
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from element import Roof, Wall, Floor, Ceiling
 import bldg_code
 from survey_data import SurveyData
 from geopy import distance
-from scipy.io import loadmat
+from interface import Interface
 import pandas as pd
 from code_pressures import PressureCalc
 
@@ -609,17 +607,3 @@ class Space(Zone):
         new_zone = self
         Zone.__init__(self, new_zone)
         self.hasName = None
-
-
-class Interface:
-    def __init__(self, first_instance, second_instance):
-        # An interface is the surface where two building elements: 2 zones or 1 element + 1 zone meet
-        self.isInterfaceOf = [first_instance, second_instance]
-        # Attributes outside of the BOT Ontology:
-        # Interfaces like connections can have a 3D Model and capacity:
-        self.hasAnalysisModel = None
-        self.hasFixity = None  # typ. options: fixed, pinned, roller, free
-        self.hasCapacity = {'type': None, 'value': None}
-        self.hasLoadingDemand = {'type': None, 'value': None}
-        self.hasFailure = None
-        self.hasType = None  # options here are point or plane
