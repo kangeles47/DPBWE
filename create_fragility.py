cb8594bf06b8093d89b2fc384476d9d123ce1e95
kangeles47 <kangeles47@gmail.com>
2021-02-22 16:29:02 -0500

    Skeleton for building permit data

--- create_fragility.py
+++ create_fragility.py
@@ -30,6 +30,39 @@ def add_steer_data(bldg, parcel_identifier, steer_file_path):
         pass
 
 
+def add_permit_data(bldg, df_inventory, parcel_identifier, dis_permit_file_path, permit_file_path=None):
+    # Permit data can be leveraged to inform:
+    # (1) the presence of damage (disaster permits) or
+    # (2) the presence of a retrofit (e.g., re-roofing)
+    # To bring in permit data, there needs to be a way to map permit number to parcel
+    # E.g., the permit may be listed in the parcel's property listing or
+    # the permit database may have the parcel's address
+    # Load the disaster permit data:
+    if dis_permit_file_path is not None:
+        df_dis_permit = pd.read_csv(dis_permit_file_path)
+    # Load the regular permit data:
+    if permit_file_path is not None:
+        df_permit = pd.read_csv(permit_file_path)
+    # Find permit descriptions for the parcel:
+    if parcel_identifier is not None:  # Address or parcel number match
+        if dis_permit_file_path is not None:
+            dis_permits = df_dis_permit.loc[df_dis_permit['SITE_ADDR'] == parcel_identifier]
+        if permit_file_path is not None:
+            permits = df_permit.loc[df_permit['SITE_ADDR'] == parcel_identifier]
+    else:  # Permit number match
+        # Access the parcel's list of permits:
+        for p in bldg.hasPermit:
+            # Find the permit descriptions:
+            if 'DIS' in p and dis_permit_file_path is not None:
+                dis_permits = df_dis_permit.loc[df_dis_permit['Permit Number'] == p]
+            else:
+                if 'DIS' not in p and permit_file_path is not None:
+                    permits = df_permit.loc[df_permit['Permit Number'] == p]
+    # Use the permit description to determine the damage type or a retrofit condition:
+
+
+
+
 def find_damage_data(bldg_identifier, component_type, steer_flag, crowd_sourced_flag, fema_modeled_flag, fema_claims_flag, imagery_postp_flag, ind_recon_flag, bldg_permit_flag):
     data_details_dict = {'Damage Precision': {'type': None, 'value': None},
                          'Location Precision': None}
