b6c00c59b95eda072d4db8d338b339e5f2520832
kangeles47 <kangeles47@gmail.com>
2021-03-01 11:57:26 -0500

    Setting up for initialization

--- create_fragility.py
+++ create_fragility.py
@@ -1,7 +1,8 @@
 import pandas as pd
-import zone
 import get_sim_bldgs
 import post_disaster_damage_data_source
+from OBDM.zone import Site
+from parcel import Parcel
 
 
 def create_fragility(bldg, site, component_type, hazard_type, event_year, event_name, data_types, file_paths):
@@ -16,3 +17,11 @@ def create_fragility(bldg, site, component_type, hazard_type, event_year, event_
             data_details = data_types[i].add_permit_data(bldg, component_type, hazard_type, file_paths[i])
         data_list.append(data_details)
     # Step 3: Choose the best data source for this damage description:
+
+
+# Initialization:
+site = Site()
+lon = -85.676188
+lat = 30.190142
+test = Parcel('12345', 4, 'Financial', 1989, '1002 23RD ST W PANAMA CITY 32405', 41134, lon, lat)
+bldg = Parcel()
