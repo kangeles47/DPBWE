bfa0aeea2c3d29756ebc7eaf9b2f88747fa071a6
kangeles47 <kangeles47@gmail.com>
2021-03-01 17:04:58 -0500

    Setting up workflow

--- create_fragility.py
+++ create_fragility.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import get_sim_bldgs
 import post_disaster_damage_data_source
-from OBDM.zone import Site
+from OBDM.zone import Site, Building
+from OBDM.element import Roof
 from parcel import Parcel
 
 
@@ -19,9 +20,35 @@ def create_fragility(bldg, site, component_type, hazard_type, event_year, event_
     # Step 3: Choose the best data source for this damage description:
 
 
-# Initialization:
+# Create a Site Class holding all of the data models for the parcels:
+inventory = 'C:/Users/Karen/PycharmProjects/DPBWE/BayCountyCommercialParcels.csv'
+df = pd.read_csv(inventory)
 site = Site()
-lon = -85.676188
-lat = 30.190142
-test = Parcel('12345', 4, 'Financial', 1989, '1002 23RD ST W PANAMA CITY 32405', 41134, lon, lat)
-bldg = Parcel()
+for row in range(0, len(df.index[0:5])):
+    pid = df['Parcel Id'][row]
+    num_stories = df['Stories'][row]
+    use_code = df['Use Code'][row]
+    year_built = df['Year Built'][row]
+    address = df['Address'][row]
+    area = df['Square Footage'][row].replace(',','')
+    lon = -85.647660  # df['Longitude'][row]
+    lat = 30.159210  # df['Latitude'][row]
+    new_bldg = Building()
+    new_bldg.add_parcel_data(pid, num_stories, use_code, year_built, address, area, lon, lat)
+    #new_bldg = Parcel(pid, num_stories, use_code, year_built, address, area, lon, lat)
+    new_bldg.hasElement['Roof'] = [Roof()]
+    # Add additional attributes:
+    new_bldg.hasElement['Roof'][0].hasCover = df['Roof Cover'][row]
+    new_bldg.hasElement['Roof'][0].hasShape = 'flat'
+    new_bldg.hasElement['Roof'][0].hasPitch = 0
+    new_bldg.hasGeometry['Height'] = 40
+    site.hasBuilding.append(new_bldg)
+site.update_zones()
+site.update_elements()
+# Test out data extraction with one parcel:
+rcover = 'POLY TPO'
+data_types = [post_disaster_damage_data_source.STEER()]
+file_paths = ['C:/Users/Karen/Desktop/HM_D2D_Building.csv']
+bldg = Parcel('21084-010-000', 6, 'PROFESSION (001900)', 1987, '801 6TH ST E PANAMA CITY 32401', '70788', -85.647660, 30.159210)
+bldg.hasElement['Roof'][0].hasCover = rcover
+create_fragility(bldg, site, component_type='roof cover', hazard_type='wind', event_year=2018, event_name='Hurricane Michael', data_types=data_types, file_paths=file_paths)
