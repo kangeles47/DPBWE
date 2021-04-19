b07c5b951bc7dd698e180c5ea8c12ae90002daa2
kangeles47 <kangeles47@gmail.com>
2021-04-19 07:08:11 -0700

    briandumping bayesian code

--- execute_bayesian.py
+++ execute_bayesian.py
@@ -1,24 +1,31 @@
 import pymc3 as pm
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import norm
 
-alphas = np.array([1, 1, 1])
-c = np.array([3, 2, 1])
 
-# Create model
+# Damage observations per building and IM are either 0 or 1 (failure)
+def custom_logp(mu, beta, n_i, N_i, im_i):
+    return  sum(n_i*np.log(norm.cdf(np.log(im_i), np.log(mu), beta)) + (N_i-n_i)*np.log(1-norm.cdf(np.log(im_i), np.log(mu), beta)))
+
+
+# Generate the dataset (observed values):
+# Note: no DataFrames, no Booleans
+
+# Create Bayesian model:
 with pm.Model() as model:
     # Set up your prior:
-    # Parameters of the Multinomial are from a Dirichlet
-    parameters = pm.Dirichlet('parameters', a=alphas, shape=3)
-    # Set up the likelihood function:
-    # Observed data is from a Multinomial distribution
-    observed_data = pm.Multinomial(
-        'observed_data', n=6, p=parameters, shape=3, observed=c)
-
-with model:
-    # Sample from the posterior
-    trace = pm.sample(draws=1000, chains=2, tune=500,
-                      discard_tuned_samples=True)
+    mu = pm.Normal('mu', 0, 0.001, value=0)  # check these
+    beta = pm.Normal('beta', 0, 0.001, value=0)
+    # Set up log-likelihood function:
+    param_obs = pm.DensityDist('param_obs', custom_logp(mu, beta, n_i, N_i, im_i), observed=0)
+    # Determine the posterior
+    trace = pm.sample(1000)  # might want to include a burn-in perior here
+    # Plot the posterior distributions of each RV
+    pm.traceplot(trace, ['mu', 'beta'])
+    pm.summary(trace)
+    plt.show()
 
 def bayesian_updating(name, priors_beta_mu1, priors_beta_sd1, priors_beta_mu0, priors_beta_sd0):
     data = pd.read_csv('file_path')  # 'file_path' contains observation data
@@ -38,5 +45,5 @@ def bayesian_updating(name, priors_beta_mu1, priors_beta_sd1, priors_beta_mu0, p
     return updating
 
 def get_x(x):
-    input_value = 
+    input_value =
     calc_x = np.exp(input_val)
