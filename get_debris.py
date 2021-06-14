726aeb29372492a15638ee414e4786e6a312f7d5
kangeles47 <kangeles47@gmail.com>
2021-06-14 12:17:21 -0400

    Defining rvs for flight trajectory distances

--- get_debris.py
+++ get_debris.py
@@ -1,4 +1,5 @@
 from pandas import read_csv
+from scipy.stats import norm, uniform
 # Create decision trees to characterize missile environment and consequent debris trajectories
     # Might want to include here consideration of roof assembly condition (age)
     # Typical debris types: roof covers, roof sheathing, frame/joist elements (e.g., timber)
@@ -6,13 +7,24 @@ from pandas import read_csv
 # Map debris classes to appropriate trajectory models
 # Develop decision trees to extract relevant component data from manufacturer's specifications to refine site-specific debris models
 # Implement similitude parameters to project source building damage
-def get_source_bldgs(bldg, site):
+
+
+def get_source_bldgs(bldg, site, wind_direction):
     """
 
     :param bldg: A Building object with roof information and local wind speed values
     :param site: A Site object with Building objects as described above (Site.hasBuilding)
     :return:
     """
+    # Given the wind direction, exclude all buildings downwind from the target building:
+    if wind_direction == 0:
+        pass
+    elif wind_direction == 90:
+        pass
+    elif wind_direction == 180:
+        pass
+    elif wind_direction == 270:
+        pass
     debris_type = {'roof cover': [], 'roof sheathing': [], 'roof member': []}  # 3 most common debris types
     struct_type = ['WOOD', 'JOIST', 'OWSJ', 'TRUSS']  # global list of structural types with frame members
     for i in site.hasBuilding:
@@ -28,6 +40,8 @@ def get_source_bldgs(bldg, site):
             debris_type['roof member'].append(i.adjacentElement['Roof'][0].hasStructureType)
         else:
             pass
+    # Calculate the maximum trajectory for the available debris types:
+
     # Get the trajectory for each debris type:
     for key in debris_type:
         bldg.adjacentElement['Roof'][0].hasDebrisTrajectory = get_trajectory(bldg)
@@ -42,13 +56,14 @@ def get_trajectory(bldg):
     elif bldg.hasGeometry['Length Unit'] == 'ft':
         air_density = 0.0765  # lb/ft^3
         gravity = 32.2  # ft/s^2
-    # Calculate the trajectory for this building's available debris types:
+    # Figure out what debris types are available for this building:
     if bldg.adjacentElement['Roof'][0].hasCover is None:
         traj_dict['roof cover'].append(None)
     if bldg.adjacentElement['Roof'][0].hasSheathing is None:
         traj_dict['roof sheathing'].append(None)
     if bldg.adjacentElement['Roof'][0].hasStructureType is None:
         traj_dict['roof member'].append(None)
+    # Populate parameters for stochastic flight trajectory model:
     for key in traj_dict:
         if len(traj_dict[key]) > 0:
             pass
@@ -64,13 +79,22 @@ def get_trajectory(bldg):
                 param_dict = get_traj_params(debris_class)
                 # Calculate Tachikawa number:
                 tachikawa_num = air_density*(bldg.adjacentElement['Roof'][0].hasDemand['wind speed']**2)/(2*debris_mass*gravity)
-                # Calculate mean of alongwind distance:
-                alongwind_dist = (2*debris_mass/air_density)*((0.5*c*(tachikawa_num*flight_time)**2) + (c1*(tachikawa_num*flight_time)**3) + (c2*(tachikawa_num*flight_time)**4) + (c3*(tachikawa_num*flight_time)**5))
+                # Populate coefficients and flight time RV:
+                c, c1, c2, c3, flight_time = param_dict['c'], param_dict['c1'], param_dict['c2'], param_dict['c3'], param_dict['flight_time']
+                norm_time = flight_time.rvs(size=(1, 1))*gravity/bldg.adjacentElement['Roof'][0].hasDemand['wind speed']  # or use basic wind speed at site
+                # Calculate mu_x (mean of alongwind distance):
+                alongwind_mean = (2*debris_mass/air_density)*((0.5*c*(tachikawa_num*norm_time)**2) + (c1*(tachikawa_num*norm_time)**3) + (c2*(tachikawa_num*norm_time)**4) + (c3*(tachikawa_num*norm_time)**5))
                 # Initialize remaining distribution parameters:
-                acrosswind_dist = 0
-                sigma_along = 0.35*alongwind_dist
-                sigma_across = 0.35*alongwind_dist
-    return alongwind_dist, acrosswind_dist, sigma_along, sigma_across, tachikawa_num, c, gravity
+                sigma_along = 0.35*alongwind_mean
+                sigma_across = 0.35*alongwind_mean
+                # Current data availability --> alongwind and across wind displacements are independent:
+                alongwind_dist = norm.rvs(loc=alongwind_mean, scale=sigma_along, size=(20, 1))
+                acrosswind_dist = norm.rvs(loc=0, scale=sigma_across, size=(20, 1))
+                # Add to building data model:
+                bldg.hasDebrisTrajectory[key]['alongwind'], bldg.adjacentElement['Roof'][0].hasDebrisTrajectory[key]['alongwind'] = alongwind_dist, alongwind_dist
+                bldg.hasDebrisTrajectory[key]['acrosswind'], bldg.adjacentElement['Roof'][0].hasDebrisTrajectory[key]['acrosswind'] = acrosswind_dist, acrosswind_dist
+
+    return tachikawa_num, c, gravity
 
 
 def get_debris_class(debris_type, bldg):
@@ -121,19 +145,29 @@ def get_debris_mass(bldg, debris_class):
 
 
 def get_traj_params(debris_class):
-    param_dict = {'C': None, 'c1': None, 'c2': None, 'c3': None}
+    param_dict = {'c': None, 'c1': None, 'c2': None, 'c3': None, 'flight time': None}
     # Find flight time and coefficients for the debris class:
     if debris_class == 'sheet':
-        from scipy.stats import uniform
-        flight_time = uniform(1, 2.5)  # This will be a uniform distribution
-        param_dict['C'] = 0.91
+        param_dict['flight_time'] = uniform(1, 2.5)  # This will be a uniform distribution
+        param_dict['c'] = 0.911
         param_dict['c1'] = -0.148
         param_dict['c2'] = 0.024
         param_dict['c3'] = -0.0014
     elif debris_class == 'rod':
-        c = 0.801
-    elif debris_class == 'compact':
-        pass
+        param_dict['c'] = 0.801
+        param_dict['c1'] = -0.16
+        param_dict['c2'] = 0.036
+        param_dict['c3'] = -0.0032
+    elif debris_class == 'spheres' or debris_class == 'compact':
+        param_dict['c'] = 0.496
+        param_dict['c1'] = 0.084
+        param_dict['c2'] = -0.1
+        param_dict['c3'] = 0.006
+    elif debris_class == 'cube':
+        param_dict['c'] = 0.809
+        param_dict['c1'] = -0.036
+        param_dict['c2'] = -0.052
+        param_dict['c3'] = 0.008
     return param_dict
 
 def calc_impact_momentum(debris_mass, debris_area, horiz_impact_vel):
