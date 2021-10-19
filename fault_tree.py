42e8b1cdad993fd21a341b3add4e5d46e3d20fcd
kangeles47 <kangeles47@gmail.com>
2021-10-19 16:16:39 -0400

    Braindumping code pressures and capacity code

--- fault_tree.py
+++ fault_tree.py
@@ -2,20 +2,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tpu_pressures import calc_tpu_pressures, convert_to_tpu_wdir
 from parcel import Parcel
+from bldg_code import ASCE7
 
 
-# Start with building-level fault tree:
-def sim_bldg_pressure_response(bldg, wind_speed, wind_direction, tpu_flag, cc_flag, mwfrs_flag):
+def populate_code_capacities(bldg, cc_flag, mwfrs_flag, exposure, wind_speed):
+    # Populate code-informed capacities:
+    asce7 = ASCE7(bldg, loading_flag=True)
+    if cc_flag:
+        a = asce7.get_cc_zone_width(bldg)
+        roof_flag = True
+        zone_pts, int_poly, zone2_polys = asce7.find_cc_zone_points(bldg, a, roof_flag, asce7.hasEdition)
+        asce7.assign_wcc_pressures(bldg, zone_pts, asce7.hasEdition, exposure, wind_speed)
+        asce7.assign_rcc_pressures(test, zone_pts, int_poly, asce7.hasEdition, exposure, wind_speed)
+    if mwfrs_flag:
+        asce7.assign_rmwfrs_pressures(test, asce7.hasEdition, exposure, wind_speed)
+
+
+def generate_pressure_loading(bldg, wind_speed, wind_direction, exposure, tpu_flag, cc_flag, mwfrs_flag):
     # Populate envelope pressures:
     if tpu_flag:
         # Convert wind direction to TPU wind direction:
         tpu_wdir = convert_to_tpu_wdir(wind_direction, bldg)
         key = 'local'
         edition = 'ASCE 7-16'
-        exposure = 'B'
         cat = 2
         hpr = True
+        # Find TPU wind pressures:
         df_tpu_pressures = calc_tpu_pressures(bldg, key, tpu_wdir, wind_speed, exposure, edition, cat, hpr)
+    else:
+        # Populate code-informed pressures:
+        asce7 = ASCE7(bldg, loading_flag=True)
+        if cc_flag:
+            a = asce7.get_cc_zone_width(bldg)
+            roof_flag = True
+            zone_pts, int_poly, zone2_polys = asce7.find_cc_zone_points(bldg, a, roof_flag, asce7.hasEdition)
+            asce7.assign_wcc_pressures(bldg, zone_pts, asce7.hasEdition, exposure, wind_speed)
+            asce7.assign_rcc_pressures(test, zone_pts, int_poly, asce7.hasEdition, exposure, wind_speed)
+        if mwfrs_flag:
+            asce7.assign_rmwfrs_pressures(test, asce7.hasEdition, exposure, wind_speed)
+
+
 
 # Asset Description
 # Parcel Models
@@ -26,4 +52,5 @@ test.hasElement['Roof'][0].hasShape['flat'] = True
 test.hasElement['Roof'][0].hasPitch = 0
 wind_speed = 120
 wind_direction = 45
-sim_bldg_pressure_response(test, wind_speed, wind_direction, tpu_flag=True, cc_flag=False, mwfrs_flag=False)
+exposure = 'B'
+generate_pressure_loading(test, wind_speed, wind_direction, exposure, tpu_flag=True, cc_flag=False, mwfrs_flag=False)
