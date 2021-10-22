c2837ad4d7195014901d4eaa80d1a97db1780923
kangeles47 <kangeles47@gmail.com>
2021-10-22 14:57:05 -0400

    Braindumping code to assign pressures to envelope components

--- fault_tree.py
+++ fault_tree.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 from tpu_pressures import calc_tpu_pressures, convert_to_tpu_wdir
 from parcel import Parcel
 from bldg_code import ASCE7
@@ -32,6 +33,35 @@ def generate_pressure_loading(bldg, wind_speed, wind_direction, exposure, tpu_fl
         hpr = True
         # Find TPU wind pressures:
         df_tpu_pressures = calc_tpu_pressures(bldg, key, tpu_wdir, wind_speed, exposure, edition, cat, hpr)
+        # Map pressures to specific elements on building envelope:
+        # Start with facade components:
+        roof_indices = df_tpu_pressures.loc[df_tpu_pressures['Surface Number'] == 5].index
+        df_facade = df_tpu_pressures.drop(roof_indices)
+        for wall in bldg.adjacentElement['Walls']:
+            wall_pressures = pd.DataFrame(columns=df_facade.columns)
+            for idx in df_facade.index:
+                if df_facade['Real Life Location'][idx].within(wall.hasGeometry['3D Geometry']['local']) or df_facade['Real Life Location'][idx].intersects(wall.hasGeometry['3D Geometry']['local']):
+                    # Check if this pressure is going to be mapped to a window element:
+                    if wall.hasSubElement is not None:
+                        pass
+                    else:
+                        pass
+                    wall_pressures = wall_pressures.append(df_facade.iloc[idx], ignore_index=True)
+                else:
+                    pass
+            wall.hasDemand['wind pressure']['external'] = wall_pressures
+        # Map pressures onto roof elements:
+        df_roof = df_tpu_pressures.iloc[roof_indices]
+        if len(bldg.adjacentElement['Roof'][0].hasSubElement['cover']) > 0:
+            for subelem in bldg.adjacentElement['Roof'][0].hasSubElement['cover']:
+                roof_pressures = pd.DataFrame(columns=df_roof.columns)
+                for idx in df_roof.index:
+                    if df_roof['Real Life Location'][idx].within(subelem.hasGeometry['3D Geometry']['local']) or df_roof['Real Life Location'][idx].intersects(subelem.hasGeometry['3D Geometry']['local']):
+                        roof_pressures = roof_pressures.append(df_roof.iloc[idx], ignore_index=True)
+                    else:
+                        pass
+        else:
+            bldg.adjacentElement['Roof'][0].hasDemand['wind pressure']['external'] = df_roof
     else:
         # Populate code-informed pressures:
         asce7 = ASCE7(bldg, loading_flag=True)
@@ -59,5 +89,5 @@ exposure = 'B'
 cc_flag, mwfrs_flag = True, True
 #test.hasGeometry['Height'] = 9*4
 #test.hasGeometry['Height'] = 9
-populate_code_capacities(test, cc_flag, mwfrs_flag, exposure)
-#generate_pressure_loading(test, wind_speed, wind_direction, exposure, tpu_flag=True, cc_flag=False, mwfrs_flag=False)
+#populate_code_capacities(test, cc_flag, mwfrs_flag, exposure)
+generate_pressure_loading(test, wind_speed, wind_direction, exposure, tpu_flag=True, cc_flag=False, mwfrs_flag=False)
