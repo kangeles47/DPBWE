9db4a0cb78bcde3d5eddf28478654aaddde91299
kangeles47 <kangeles47@gmail.com>
2021-10-25 14:20:33 -0400

    Mapping to building envelope components

--- fault_tree.py
+++ fault_tree.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from shapely.geometry import Polygon
+from shapely.geometry import Polygon, Point
 from tpu_pressures import calc_tpu_pressures, convert_to_tpu_wdir
 from parcel import Parcel
 from bldg_code import ASCE7
@@ -38,66 +38,71 @@ def generate_pressure_loading(bldg, wind_speed, wind_direction, exposure, tpu_fl
         # Start with facade components:
         roof_indices = df_tpu_pressures.loc[df_tpu_pressures['Surface Number'] == 5].index
         df_facade = df_tpu_pressures.drop(roof_indices)
-        fig = plt.figure()
-        ax = plt.axes(projection='3d')
+        # Add empty DataFrames to facade elements:
         for wall in bldg.adjacentElement['Walls']:
-            wall_pressures = pd.DataFrame(columns=df_facade.columns)
-            for idx in df_facade.index:
-                # Use the point geometry to buffer it out for a distance:
-                # buff_pt = df_facade['Real Life Location'][idx].buffer(distance=2)
-                # xb, yb = buff_pt.exterior.xy
-                # pts_xyz = []
-                # for j in range(0, len(xb)):
-                #     pts_xyz.append((xb[j], yb[j], df_facade['Real Life Location'][idx].z))
-                # buff_poly = Polygon(pts_xyz)
-                if df_facade['Real Life Location'][idx].within(wall.hasGeometry['3D Geometry']['local']) or df_facade['Real Life Location'][idx].intersects(wall.hasGeometry['3D Geometry']['local']):
-                    # Check if this pressure is going to be mapped to a window element:
-                    if wall.hasSubElement is not None:
-                        pass
-                    else:
-                        pass
-                    wall_pressures = wall_pressures.append(df_facade.iloc[idx], ignore_index=True)
-                else:
-                    # Buffer the point:
-                    # Use the point geometry to buffer it out for a distance:
-                    buff_pt = df_facade['Real Life Location'][idx].buffer(distance=3)
-                    xb, yb = buff_pt.exterior.xy
-                    pts_xyz = []
-                    for j in range(0, len(xb)):
-                        pts_xyz.append((xb[j], yb[j], df_facade['Real Life Location'][idx].z))
-                    buff_poly = Polygon(pts_xyz)
-                    if buff_poly.intersects(wall.hasGeometry['3D Geometry']['local']):
-                        wall_pressures = wall_pressures.append(df_facade.iloc[idx], ignore_index=True)
-                    else:
-                        pass
-            wall.hasDemand['wind pressure']['external'] = wall_pressures
-            wall_points = list(wall.hasGeometry['3D Geometry']['local'].exterior.coords)
-            xw, yw, zw = [], [], []
-            for w in wall_points:
-                xw.append(w[0])
-                yw.append(w[1])
-                zw.append(w[2])
-            if len(wall_pressures) > 0:
-                ax.plot(xw, yw, zw, 'r')
-            else:
-                ax.plot(xw, yw, zw, 'k')
+            wall.hasDemand['wind pressure']['external'] = pd.DataFrame(columns=df_facade.columns)
+        # Set up plotting:
+        # fig = plt.figure()
+        # ax = plt.axes(projection='3d')
+        no_map = []
         for idx in df_facade.index:
-            point = df_facade['Real Life Location'][idx]
-            xyz = list(point.coords)[0]
-            ax.scatter(xyz[0], xyz[1], xyz[2])
-        plt.show()
-        # Map pressures onto roof elements:
+            map_flag = False
+            ptap_loc = df_facade['Real Life Location'][idx]
+            for story in bldg.hasStory:
+                if story.hasElevation[0] <= ptap_loc.z <= story.hasElevation[1]:
+                    for wall in story.adjacentElement['Walls']:
+                        wall_pts = list(wall.hasGeometry['3D Geometry']['local'].exterior.coords)
+                        xw, yw, zw = [], [], []
+                        for w in wall_pts:
+                            xw.append(w[0])
+                            yw.append(w[1])
+                            zw.append(w[2])
+                        bound_poly = Polygon([(max(xw), max(yw)), (min(xw), max(yw)), (min(xw), min(yw)), (max(xw), min(yw))])
+                        if Point(ptap_loc.x, ptap_loc.y).within(bound_poly) or Point(ptap_loc.x, ptap_loc.y).intersects(bound_poly):
+                            if min(zw) <= ptap_loc.z <= max(zw):
+                                wall.hasDemand['wind pressure']['external'] = wall.hasDemand['wind pressure']['external'].append(df_facade.iloc[idx], ignore_index=True)
+                                map_flag = True
+                                break
+                            else:
+                                print('Point within boundary but not wall height')
+                        else:
+                            pass
+                else:
+                    pass
+            if not map_flag:
+                no_map.append(df_facade.iloc[idx])
+                #ax.scatter(ptap_loc.x, ptap_loc.y, ptap_loc.z, 'r')
+        # for wall in bldg.adjacentElement['Walls']:
+        #     wall_pts = list(wall.hasGeometry['3D Geometry']['local'].exterior.coords)
+        #     xw, yw, zw = [], [], []
+        #     for w in wall_pts:
+        #         xw.append(w[0])
+        #         yw.append(w[1])
+        #         zw.append(w[2])
+        #     ax.plot(xw, yw, zw, 'k')
+        # plt.show()
+        # Set up empty DataFrames for roof or roof_subelements:
         df_roof = df_tpu_pressures.iloc[roof_indices]
-        if len(bldg.adjacentElement['Roof'][0].hasSubElement['cover']) > 0:
+        # Map pressures onto roof elements:
+        if len(bldg.adjacentElement['Roof'][0].hasSubElement['cover']) == 0:
+            # Assign the entire DataFrame to the roof:
+            bldg.adjacentElement['Roof'][0].hasDemand['wind pressure']['external'] = df_roof
+        else:
             for subelem in bldg.adjacentElement['Roof'][0].hasSubElement['cover']:
-                roof_pressures = pd.DataFrame(columns=df_roof.columns)
-                for idx in df_roof.index:
-                    if df_roof['Real Life Location'][idx].within(subelem.hasGeometry['3D Geometry']['local']) or df_roof['Real Life Location'][idx].intersects(subelem.hasGeometry['3D Geometry']['local']):
-                        roof_pressures = roof_pressures.append(df_roof.iloc[idx], ignore_index=True)
+                subelem.hasDemand['wind pressure']['external'] = pd.DataFrame(columns=df_roof.columns)
+            # Map pressures to roof subelements
+            no_map_roof = []
+            for idx in df_roof.index:
+                map_flag = False
+                rptap_loc = df_roof['Real Life Location'][idx]
+                for subelem in bldg.adjacentElement['Roof'][0].hasSubElement['cover']:
+                    if Point(rptap_loc.x, rptap_loc.y).within(subelem.hasGeometry['2D Geometry']['local']) or Point(rptap_loc.x, rptap_loc.y).intersects(subelem.hasGeometry['2D Geometry']['local']):
+                        subelem.hasDemand['wind pressure']['external'] = subelem.hasDemand['wind pressure']['external'].append(df_roof.loc[idx], ignore_index=True)
+                        map_flag = True
                     else:
                         pass
-        else:
-            bldg.adjacentElement['Roof'][0].hasDemand['wind pressure']['external'] = df_roof
+                if not map_flag:
+                    no_map_roof.append(df_roof.loc[idx])
     else:
         # Populate code-informed pressures:
         asce7 = ASCE7(bldg, loading_flag=True)
