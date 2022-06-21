0472b2600d916260b0a199bffda06957f20d985b
kangeles47 <kangeles47@gmail.com>
2022-06-21 17:24:38 -0400

    adding commercial occupancies for hazus (case study)

--- hazus_rulesets_bay_county_fl.py
+++ hazus_rulesets_bay_county_fl.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def get_hazus_occupancy(occupancy, roof_shape, frame_type):
+def get_hazus_occupancy(occupancy, roof_shape, frame_type, num_stories):
     wmuh_occupancies = ['MULTI-FAMI (000300)', 'COOPERATIV (000500)', 'HOTELS AND (003900)']
     if 'SINGLE' in occupancy:
         if roof_shape != 'FLAT':
@@ -20,12 +20,24 @@ def get_hazus_occupancy(occupancy, roof_shape, frame_type):
                 return 'WMUH'
     elif any(occ == occupancy for occ in wmuh_occupancies):
         # Multi-family homes and Multi-unit hotel/motels:
-        if 'MASONRY' in frame_type:
-            return 'MMUH'
-        elif 'STEEL' in frame_type:  # Engineered residential building
-            return 'SERL'
-        elif 'CONCRETE' in frame_type:  # Engineered residential building
-            return 'CERL'
+        if 'STEEL' in frame_type:  # engineered residential
+            return 'SERB'
+        elif 'CONCRETE' in frame_type:  # engineered residential
+            return 'CERB'
         else:
-            # Assume that this is a wood frame structural system
-            return 'WMUH'
+            if 'MASONRY' in frame_type and num_stories < 4:
+                return 'MMUH'
+            else:
+                # Assume that this is a wood frame structural system
+                return 'WMUH'
+    else:
+        # Choose from remaining commercial occupancies:
+        if 'STEEL' in frame_type:  # engineered residential
+            if num_stories == 1:
+                return 'SPMB'
+            else:
+                return 'SECB'
+        elif 'CONCRETE' in frame_type:  # engineered residential
+            return 'CECB'
+        elif 'MASONRY' in frame_type and occupancy == 'STORES, 1 (001100)':
+            return 'MECBL'
