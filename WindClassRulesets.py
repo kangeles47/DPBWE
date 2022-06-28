27be5b8e8aa35dd3404ae714b65e8a795b9f7276
kangeles47 <kangeles47@gmail.com>
2022-06-28 15:28:39 -0400

    Updating to parse BIM dict

--- WindClassRulesets.py
+++ WindClassRulesets.py
@@ -1,40 +1,40 @@
-def building_class(occupancy, roof_shape, frame_type, num_stories):
+def building_class(BIM):
     wmuh_occupancies = ['MULTI-FAMI (000300)', 'COOPERATIV (000500)', 'HOTELS AND (003900)']
-    if 'SINGLE' in occupancy:
-        if roof_shape != 'FLAT':
+    if 'SINGLE' in BIM['occupancy_class']:
+        if BIM['roof_shape'] != 'FLAT':
             # Single family homes in HAZUS can only have hip or gable roofs
-            if 'MASONRY' in frame_type:
+            if 'MASONRY' in BIM['frame_type']:
                 return 'MSF'
             else:
                 # Assume that this is a wood frame structural system
                 return 'WSF'
         else:
             # Multi-family homes can have flat roofs
-            if 'MASONRY' in frame_type:
+            if 'MASONRY' in BIM['frame_type']:
                 return 'MMUH'
             else:
                 # Assume that this is a wood frame structural system
                 return 'WMUH'
-    elif any(occ == occupancy for occ in wmuh_occupancies):
+    elif any(occ == BIM['occupancy_class'] for occ in wmuh_occupancies):
         # Multi-family homes and Multi-unit hotel/motels:
-        if 'STEEL' in frame_type:  # engineered residential
+        if 'STEEL' in BIM['frame_type']:  # engineered residential
             return 'SERB'
-        elif 'CONCRETE' in frame_type:  # engineered residential
+        elif 'CONCRETE' in BIM['frame_type']:  # engineered residential
             return 'CERB'
         else:
-            if 'MASONRY' in frame_type and num_stories < 4:
+            if 'MASONRY' in BIM['frame_type'] and BIM['num_stories'] < 4:
                 return 'MMUH'
             else:
                 # Assume that this is a wood frame structural system
                 return 'WMUH'
     else:
         # Choose from remaining commercial occupancies:
-        if 'STEEL' in frame_type:  # engineered residential
-            if num_stories == 1:
+        if 'STEEL' in BIM['frame_type']:  # engineered residential
+            if BIM['num_stories'] == 1:
                 return 'SPMB'
             else:
                 return 'SECB'
-        elif 'CONCRETE' in frame_type:  # engineered residential
+        elif 'CONCRETE' in BIM['frame_type']:  # engineered residential
             return 'CECB'
-        elif 'MASONRY' in frame_type and occupancy == 'STORES, 1 (001100)':
+        elif 'MASONRY' in BIM['frame_type'] and BIM['occupancy_class'] == 'STORES, 1 (001100)':
             return 'MECBL'
