563f763dff12fbce3667ed9a1603d4c54b1c38ff
kangeles47 <kangeles47@gmail.com>
2022-06-28 16:33:34 -0400

    bldg_config figured out for WSF and WMUH

--- run_hazus_dl.py
+++ run_hazus_dl.py
@@ -1,14 +1,16 @@
 import pandas as pd
 from WindMetaVarRulesets import get_meta_var
 from WindClassRulesets import building_class
+from WindWSFRulesets import wsf_config
+from WindWMUHRulesets import wmuh_config
 
 
 file_path = 'D:/Users/Karen/Documents/Github/DPBWE/Asset_Description_PC_FL.csv'
 hazus_classes = []
 column_names = ['id', 'Latitude', 'Longitude', 'BldgID', 'Address', 'City', 'county',
        'State', 'occupancy_class', 'frame_type', 'year_built',
-       'num_stories', 'NoUnits', 'PlanArea', 'flood_zone', 'V_ult', 'lulc', 'WindZone', 'AvgJanTemp', 'RoofShape', 'RoofSlope',
-       'RoofCover', 'RoofSystem', 'MeanRoofHt', 'WindowArea', 'Garage',
+       'stories', 'NoUnits', 'PlanArea', 'flood_zone', 'V_ult', 'lulc', 'WindZone', 'AvgJanTemp', 'roof_shape', 'RoofSlope',
+       'RoofCover', 'RoofSystem', 'MeanRoofHt', 'WindowArea', 'garage_tag',
        'HazusClassW', 'AnalysisDefault', 'AnalysisAdopted', 'Modifications',
        'z0', 'structureType', 'replacementCost', 'Footprint',
        'HazardProneRegion', 'WindBorneDebris', 'SecondaryWaterResistance',
@@ -24,3 +26,15 @@ for idx in df_inventory.index.to_list():
     BIM = get_meta_var(BIM)
     # Identify HAZUS building class:
     bldg_class = building_class(BIM)
+    # Using HAZUS building class, find the remaining archetype variables:
+    if bldg_class == 'WSF':
+        bldg_config = wsf_config(BIM)
+    elif bldg_class == 'WMUH':
+        bldg_config = wmuh_config(BIM)
+    else:
+        bldg_config = 'NONE'
+        # try:
+        #     print('Rulesets for this building class not yet defined: ' + bldg_class)
+        # except TypeError:
+        #     bldg_class = 'NONE'
+    print(bldg_config)
