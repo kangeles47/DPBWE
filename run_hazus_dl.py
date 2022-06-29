5327fac018477197b6fa66434c6752c016b53f79
kangeles47 <kangeles47@gmail.com>
2022-06-29 17:14:07 -0400

    cleaning up data types for rulesets, adding bldg_config exception

--- run_hazus_dl.py
+++ run_hazus_dl.py
@@ -4,22 +4,25 @@ from WindClassRulesets import building_class
 from WindWSFRulesets import wsf_config
 from WindWMUHRulesets import wmuh_config
 
-
 file_path = 'D:/Users/Karen/Documents/Github/DPBWE/Asset_Description_PC_FL.csv'
-hazus_classes = []
 column_names = ['id', 'Latitude', 'Longitude', 'BldgID', 'Address', 'City', 'county',
-       'State', 'occupancy_class', 'frame_type', 'year_built',
-       'stories', 'NoUnits', 'PlanArea', 'flood_zone', 'V_ult', 'lulc', 'WindZone', 'AvgJanTemp', 'roof_shape', 'RoofSlope',
-       'RoofCover', 'RoofSystem', 'MeanRoofHt', 'WindowArea', 'garage_tag',
-       'HazusClassW', 'AnalysisDefault', 'AnalysisAdopted', 'Modifications',
-       'z0', 'structureType', 'replacementCost', 'Footprint',
-       'HazardProneRegion', 'WindBorneDebris', 'SecondaryWaterResistance',
-       'RoofQuality', 'RoofDeckAttachmentW', 'Shutters', 'TerrainRoughness']
+                'State', 'occupancy_class', 'frame_type', 'year_built',
+                'stories', 'NoUnits', 'PlanArea', 'flood_zone', 'V_ult', 'lulc', 'WindZone', 'AvgJanTemp', 'roof_shape',
+                'roof_slope', 'RoofCover', 'RoofSystem', 'MeanRoofHt', 'WindowArea', 'garage_tag',
+                'HazusClassW', 'AnalysisDefault', 'AnalysisAdopted', 'Modifications',
+                'z0', 'structureType', 'replacementCost', 'Footprint',
+                'HazardProneRegion', 'WindBorneDebris', 'SecondaryWaterResistance',
+                'RoofQuality', 'RoofDeckAttachmentW', 'Shutters', 'TerrainRoughness']
 df_inventory = pd.read_csv(file_path, names=column_names, header=0)
 # Cleaning up some data types:
 df_inventory['year_built'].apply(int)
 df_inventory['lulc'].apply(int)
+df_inventory['roof_shape'] = df_inventory['roof_shape'].str.lower()
+# Replacing roof shape names to SimCenter naming convention:
+df_inventory['roof_shape'] = df_inventory['roof_shape'].replace(['flat'], 'flt')
+df_inventory['roof_shape'] = df_inventory['roof_shape'].replace(['gable'], 'gab')
 # Find the HAZUS archetype for each building in the given inventory:
+hazus_archetypes = []
 for idx in df_inventory.index.to_list():
     BIM = df_inventory.iloc[idx].to_dict()
     # Set up additional meta-variables:
@@ -37,4 +40,8 @@ for idx in df_inventory.index.to_list():
         #     print('Rulesets for this building class not yet defined: ' + bldg_class)
         # except TypeError:
         #     bldg_class = 'NONE'
-    print(bldg_config)
+    if bldg_config != 'NONE':
+        hazus_archetypes.append(bldg_config)
+    else:
+        pass
+print(hazus_archetypes)
