448b64e62464fb654328d52f8ceec918a3bc79ec
kangeles47 <kangeles47@gmail.com>
2022-07-06 20:13:38 -0400

    removing roof shape condition for wsf

--- HAZUS_style_DL/WindClassRulesets.py
+++ HAZUS_style_DL/WindClassRulesets.py
@@ -2,20 +2,12 @@ def building_class(BIM):
     wmuh_occupancies = ['MULTI-FAMI (000300)', 'COOPERATIV (000500)', 'HOTELS AND (003900)']
     comm_eng_occupancies = ['OFFICE BLD (001700)', 'STORES, 1 (001100)', 'DRIVE-IN R (002200)']
     if BIM['occupancy_class'] == 'SINGLE FAM (000100)':
-        if BIM['roof_shape'] != 'flt':
-            # Single family homes in HAZUS can only have hip or gable roofs
-            if 'MASONRY' in BIM['frame_type']:
-                return 'MSF'
-            else:
-                # Assume that this is a wood frame structural system
-                return 'WSF'
+        # Single family homes in HAZUS can only have hip or gable roofs
+        if 'MASONRY' in BIM['frame_type']:
+            return 'MSF'
         else:
-            # Multi-family homes can have flat roofs
-            if 'MASONRY' in BIM['frame_type']:
-                return 'MMUH'
-            else:
-                # Assume that this is a wood frame structural system
-                return 'WMUH'
+            # Assume that this is a wood frame structural system
+            return 'WSF'
     elif any(occ == BIM['occupancy_class'] for occ in wmuh_occupancies):
         # Multi-family homes and Multi-unit hotel/motels:
         if 'STEEL' in BIM['frame_type']:  # engineered residential
@@ -29,9 +21,10 @@ def building_class(BIM):
                 # Assume that this is a wood frame structural system
                 return 'WMUH'
     else:
+        spbm_occupancies = ['GYM (003350)', 'WAREHOUSE- (004800)']
         # Choose from remaining commercial occupancies:
         if 'STEEL' in BIM['frame_type']:  # engineered residential
-            if BIM['stories'] == 1:
+            if any(occ == BIM['occupancy_class'] for occ in spbm_occupancies) and BIM['num_stories'] == 1:
                 return 'SPMB'
             else:
                 return 'SECB'
