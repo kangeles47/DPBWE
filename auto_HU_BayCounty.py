ff92b0160205f78786b3a95ae77663e23b8640b9
kangeles47 <kangeles47@gmail.com>
2022-08-24 12:33:59 -0400

    Cleaning up auto populate function

--- auto_HU_BayCounty.py
+++ auto_HU_BayCounty.py
@@ -3,14 +3,14 @@ import numpy as np
 import datetime
 import math
 
-from MetaVarRulesets import parse_BIM
-from BldgClassRulesets import building_class
-from WindWSFRulesets import WSF_config
-from WindWMUHRulesets import WMUH_config
+from HAZUS_style_DL.WindMetaVarRulesets import parse_BIM
+from HAZUS_style_DL.WindClassRulesets import building_class
+from HAZUS_style_DL.WindWSFRulesets import WSF_config
+
 
 def auto_populate(BIM):
     """
-    Populates the DL model for hurricane assessments in Atlantic County, NJ
+    Populates the DL model for hurricane assessments in Bay County, FL
 
     Assumptions:
     - Everything relevant to auto-population is provided in the Buiding
@@ -22,28 +22,28 @@ def auto_populate(BIM):
     ----------
     BIM_in: dictionary
         Contains the information that is available about the asset and will be
-        used to auto-popualate the damage and loss model.
+        used to auto-populate the damage and loss model.
 
     Returns
     -------
     BIM_ap: dictionary
-        Containes the extended BIM data.
+        Contains the extended BIM data.
     DL_ap: dictionary
         Contains the auto-populated loss model.
     """
 
     # parse the BIM data
-    BIM_ap = parse_BIM(BIM, location="LA", hazards=['wind',])
+    BIM_ap = parse_BIM(BIM)
 
     # identify the building class
-    bldg_class = building_class(BIM_ap, hazard='wind')
+    bldg_class = building_class(BIM_ap)
     BIM_ap.update({'HazusClassW': bldg_class})
 
     # prepare the building configuration string
     if bldg_class == 'WSF':
         bldg_config = WSF_config(BIM_ap)
-    elif bldg_class == 'WMUH':
-        bldg_config = WMUH_config(BIM_ap)
+    # elif bldg_class == 'WMUH':
+    #     bldg_config = WMUH_config(BIM_ap)
     else:
         raise ValueError(
             f"Building class {bldg_class} not recognized by the "
@@ -68,13 +68,5 @@ def auto_populate(BIM):
             }]
         }
     }
-
-    # drop keys of internal variables from BIM_ap dict
-    internal_vars = ['V_ult', 'V_asd']
-    for var in internal_vars:
-        try:
-            BIM_ap.pop(var)
-        except:
-            pass
-
+    # Note: might needs to add some "Combinations field: see auto_HU_LA"
     return BIM_ap, DL_ap
