02b597dba43cb58611fed91b110e7df22ccbd79b
kangeles47 <kangeles47@gmail.com>
2022-09-27 21:08:53 -0400

    Updating with roof permit provisions

--- WindWSFRulesets.py
+++ WindWSFRulesets.py
@@ -19,80 +19,86 @@ def WSF_config(BIM):
 
     # Secondary Water Resistance (SWR)
     swr = False
-    if BIM['YearBuilt'] > 2009:  # swr required in single family homes upon 2009 supplement to 2007 FBC
+    if BIM['RoofReplaceYear'] > 2008:
         swr = True
-    elif 2007 < BIM['YearBuilt'] <= 2009:
-        if BIM['hvhz']:  # between 2007-2009, swr required in HVHZ and outside HVHZ
-            swr = True
-        else:
-            if (2/12) <= BIM['RoofSlope'] < (4/12):  # swr required for some sloped roofs
-                swr = True  # Based off of roof retrofit provisions in 2007 FBC - Existing buildings
-            else:
-                # For roof slopes >= 4/12 and not in HVHZ, use homeowner
-                # compliance data from NC Coastal Homeowner Survey (2017) to capture potential
-                # human behavior (% of sealed roofs in NC dataset).
-                swr = random.random() < 0.6
-    elif 2001 < BIM['YearBuilt'] <= 2007:
-        # For buildings built before enactment of FBC, SWR is based on homeowner
-        # compliance data from NC Coastal Homeowner Survey (2017) to capture potential
-        # human behavior (% of sealed roofs in NC dataset).
-        swr = random.random() < 0.6
     else:
-        # According to 903.2 in the 1995 CABO, for roofs with slopes between
-        # 2:12 and 4:12, an underlayment consisting of two layers of No. 15
-        # felt must be applied. In severe climates (less than or equal to 25
-        # degrees Fahrenheit average in January), these two layers must be
-        # cemented together.
-        # According to 903.3 in the 1995 CABO, roofs with slopes greater than
-        # or equal to 4:12 shall have an underlayment of not less than one ply
-        # of No. 15 felt.
-        #
-        # Similar rules are prescribed in CABO 1992, 1989, 1986, 1983
-        #
-        # Since low-slope roofs require two layers of felt, this is taken to
-        # be secondary water resistance. This ruleset is for asphalt shingles.
-        # Almost all other roof types require underlayment of some sort, but
-        # the ruleset is based on asphalt shingles because it is most
-        # conservative.
-        if BIM['RoofShape'].lower() == 'flt':  # note there is actually no 'flat'
+        if BIM['YearBuilt'] > 2008:  # swr required in single family homes upon 2009 supplement to 2007 FBC
             swr = True
-        elif BIM['RoofShape'].lower() == 'gab' or BIM['RoofShape'].lower() == 'hip':
-            if BIM['RoofSlope'] <= 0.17:
+        elif 2007 < BIM['YearBuilt'] <= 2008:
+            if BIM['hvhz']:  # between 2007-2008, swr required in HVHZ and outside HVHZ
                 swr = True
-            elif 0.17 < BIM['RoofSlope'] < 0.33:
-                swr = (BIM['avg_jan_temp'].lower() == 'below')
-            elif BIM['RoofSlope'] >= 0.33:
-                swr = False
+            else:
+                if (2/12) <= BIM['RoofSlope'] < (4/12):  # swr required for some sloped roofs
+                    swr = True  # Based off of roof retrofit provisions in 2007 FBC - Existing buildings
+                else:
+                    # For roof slopes >= 4/12 and not in HVHZ, use homeowner
+                    # compliance data from NC Coastal Homeowner Survey (2017) to capture potential
+                    # human behavior (% of sealed roofs in NC dataset).
+                    swr = random.random() < 0.6
+        elif 2001 < BIM['YearBuilt'] <= 2007:
+            # For buildings built before enactment of FBC, SWR is based on homeowner
+            # compliance data from NC Coastal Homeowner Survey (2017) to capture potential
+            # human behavior (% of sealed roofs in NC dataset).
+            swr = random.random() < 0.6
+        else:
+            # According to 903.2 in the 1995 CABO, for roofs with slopes between
+            # 2:12 and 4:12, an underlayment consisting of two layers of No. 15
+            # felt must be applied. In severe climates (less than or equal to 25
+            # degrees Fahrenheit average in January), these two layers must be
+            # cemented together.
+            # According to 903.3 in the 1995 CABO, roofs with slopes greater than
+            # or equal to 4:12 shall have an underlayment of not less than one ply
+            # of No. 15 felt.
+            #
+            # Similar rules are prescribed in CABO 1992, 1989, 1986, 1983
+            #
+            # Since low-slope roofs require two layers of felt, this is taken to
+            # be secondary water resistance. This ruleset is for asphalt shingles.
+            # Almost all other roof types require underlayment of some sort, but
+            # the ruleset is based on asphalt shingles because it is most
+            # conservative.
+            if BIM['RoofShape'].lower() == 'flt':  # note there is actually no 'flat'
+                swr = True
+            elif BIM['RoofShape'].lower() == 'gab' or BIM['RoofShape'].lower() == 'hip':
+                if BIM['RoofSlope'] <= 0.17:
+                    swr = True
+                elif 0.17 < BIM['RoofSlope'] < 0.33:
+                    swr = (BIM['avg_jan_temp'].lower() == 'below')
+                elif BIM['RoofSlope'] >= 0.33:
+                    swr = False
 
     # Roof Deck Attachment (RDA)
     rda = '6d' # Default (aka A) in Reorganized Rulesets - WIND
-    if BIM['YearBuilt'] > 2001:
-        # Table R602.2(1) of 2007 FBCR requires 8d nails with 6"/12" spacing for Vasd less that 100 mph
-        # 8d nails with 6"/6" spacing for Vasd > 100 mph.
-        if BIM['DWSII'] > 130:
-            rda = '8s'
+    if BIM['RoofReplaceYear'] > 2008:
+        rda = '8s'
+    else:
+        if BIM['YearBuilt'] > 2001:
+            # Table R602.2(1) of 2007 FBCR requires 8d nails with 6"/12" spacing for Vasd less that 100 mph
+            # 8d nails with 6"/6" spacing for Vasd > 100 mph.
+            if BIM['DWSII'] > 130:
+                rda = '8s'
+            else:
+                rda = '8d'  # 8d @ 6"/12" ('B' in the Reorganized Rulesets - WIND)
+        elif 1994 < BIM['YearBuilt'] <= 2001:
+            # 1994 SFBC: Section 2909.2 - Requires 8d nails with 6"/6" for roof sheathing (HVHZ)
+            # 1995 CABO: Table 602.3a - 8d or 6d nails at 6"/12" depending on sheathing thickness.
+            # Assign as RV for buildings outside of HVHZ.
+            if BIM['hvhz']:
+                rda = '8s'
+            else:
+                if random.random() <= 0.5:
+                    rda = '6d'  # 6d @ 6"/12" ('A' in the Reorganized Rulesets - WIND)
+                else:
+                    rda = '8d'  # 8d @ 6"/12" ('B' in the Reorganized Rulesets - WIND)
         else:
-            rda = '8d'  # 8d @ 6"/12" ('B' in the Reorganized Rulesets - WIND)
-    elif 1994 < BIM['YearBuilt'] <= 2001:
-        # 1994 SFBC: Section 2909.2 - Requires 8d nails with 6"/6" for roof sheathing (HVHZ)
-        # 1995 CABO: Table 602.3a - 8d or 6d nails at 6"/12" depending on sheathing thickness.
-        # Assign as RV for buildings outside of HVHZ.
-        if BIM['hvhz']:
-            rda = '8s'
-        else:
+            # year <= 1994
+            # SFBC and CABO both designate nailing based on sheathing thickness
+            # See e.g., 1992 SFBC: Section 2909.2 and 1992 CABO: Table R-402.3a.
+            # With no way to determine actual sheathing thickness, assign as random variable:
             if random.random() <= 0.5:
                 rda = '6d'  # 6d @ 6"/12" ('A' in the Reorganized Rulesets - WIND)
             else:
                 rda = '8d'  # 8d @ 6"/12" ('B' in the Reorganized Rulesets - WIND)
-    else:
-        # year <= 1994
-        # SFBC and CABO both designate nailing based on sheathing thickness
-        # See e.g., 1992 SFBC: Section 2909.2 and 1992 CABO: Table R-402.3a.
-        # With no way to determine actual sheathing thickness, assign as random variable:
-        if random.random() <= 0.5:
-            rda = '6d'  # 6d @ 6"/12" ('A' in the Reorganized Rulesets - WIND)
-        else:
-            rda = '8d'  # 8d @ 6"/12" ('B' in the Reorganized Rulesets - WIND)
 
     # Roof-Wall Connection (RWC)
     # 2001 FBC: Section 2321.1 - Anchorage shall be continuous from the
@@ -101,23 +107,26 @@ def WSF_config(BIM):
     # Documentation from FL's State Board of Administration indicate that straps became the standard for Floridian
     # construction after Hurricane Andrew in 1992.
     # Assume that if classified as HPR, then enhanced connection would be used.
-    if BIM['YearBuilt'] > 1992:
-        if BIM['hpr']:
-            rwc = 'strap'  # Strap
-        else:
-            rwc = 'tnail'  # Toe-nail
-    # HAZUS-HM documentation states that tie down straps have been required for rwc in Dade and Broward counties since
-    # the inception of the SFBC in the late 1950's. In Palm Beach County, rwc have been straps since late 1970's.
-    elif 1957 < BIM['YearBuilt'] <= 1992:
-        if BIM['hvhz']:
-            rwc = 'strap'  # Strap
-        else:
-            if BIM['YearBuilt'] > 1976 and BIM['county'].lower() == 'palm beach':
-                rwc = 'strap'
-            else:
-                rwc = 'tnail'
+    if BIM['RoofReplaceYear'] > 2008:
+        rwc = 'strap'
     else:
-        rwc = 'tnail'  # Assume all remaining construction uses toe-nails for rwc
+        if BIM['YearBuilt'] > 1992:
+            if BIM['hpr']:
+                rwc = 'strap'  # Strap
+            else:
+                rwc = 'tnail'  # Toe-nail
+        # HAZUS-HM documentation states that tie down straps have been required for rwc in Dade and Broward counties since
+        # the inception of the SFBC in the late 1950's. In Palm Beach County, rwc have been straps since late 1970's.
+        elif 1957 < BIM['YearBuilt'] <= 1992:
+            if BIM['hvhz']:
+                rwc = 'strap'  # Strap
+            else:
+                if BIM['YearBuilt'] > 1976 and BIM['county'].lower() == 'palm beach':
+                    rwc = 'strap'
+                else:
+                    rwc = 'tnail'
+        else:
+            rwc = 'tnail'  # Assume all remaining construction uses toe-nails for rwc
 
     # Shutters
     # FBCR 2000-2015:
